68761767d4c00b8ebd5ca4783225c65caa982aaa
shabani005 <hasan.shabani2005@gmail.com>
2025-06-26 18:04:54 +0300

    fixed installer

--- .config/installer/install.sh
+++ .config/installer/install.sh
@@ -1,4 +1,4 @@
-echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> ~/.zshrc && \
-source ~/.zshrc && \
+echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> ~/.bashrc && \
+source ~/.bashrc && \
 git clone --bare <your-repo-url> $HOME/.dotfiles && \
 dotfiles checkout
